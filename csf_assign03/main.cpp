fd5beaa7d4abeb3e9ab30223decf72589f959125
tigerding <zding27@jh.edu>
2025-03-05 12:44:27 -0500

    basic command arg handling

--- csf_assign03/main.cpp
+++ csf_assign03/main.cpp
@@ -1,7 +1,24 @@
 #include <iostream>
 
 int main( int argc, char **argv ) {
-  // TODO: implement
+  if (argc != 7) return -1;
+  
+  int load_count = 0;
+  int store_count = 0;
+
+  std::string line;
+  while (std::getline(std::cin, line)) {
+    if (line[0] == 'l') load_count++;
+    else store_count++;
+  }
+
+  printf("Total loads: %d\n", load_count);
+  printf("Total stores: %d\n", store_count);
+  printf("Load hits: %d\n", -1);
+  printf("Load misses: %d\n", -1);
+  printf("Store hits: %d\n", -1);
+  printf("Store misses: %d\n", -1);
+  printf("Total cycles: %d\n", -1);
 
   return 0;
 }
