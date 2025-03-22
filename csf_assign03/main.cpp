0f7a83fd6505d830b3af5a35f3897e74d78bb34a
tigerding <zhiyuanding01@gmail.com>
2025-03-22 17:50:02 +0000

    use cache in main

--- csf_assign03/main.cpp
+++ csf_assign03/main.cpp
@@ -1,16 +1,6 @@
 #include <iostream>
-#include <unordered_map>
-#include <vector>
 
-// Cache configuration from command args
-struct CacheConfig {
-  int num_sets;
-  int num_blocks;
-  int block_size;
-  bool write_allocate;  // 0 is no-write-allocate, 1 is write-allocate
-  bool write_through;   // 0 is write-back, 1 is write-through
-  bool is_lru;          // 0 is FIFO, 1 is LRU
-};
+#include "cache.h"
 
 /*
  * Check if a number is a power of 2.
@@ -94,25 +84,28 @@ int main(int argc, char **argv) {
   CacheConfig config;
   if (!parseArgs(argc, argv, config)) return 1;  // error termination
 
-  int load_count = 0;
-  int store_count = 0;
+  Cache cache(config);
 
   std::string line;
   while (std::getline(std::cin, line)) {
+    std::string address_str = line.substr(2, 10);
+    uint32_t address = std::stoul(address_str, nullptr, 16);
     if (line[0] == 'l') {
-      load_count++;
+      cache.load(address);
     } else {
-      store_count++;
+      cache.save(address);
     }
   }
 
-  printf("Total loads: %d\n", load_count);
-  printf("Total stores: %d\n", store_count);
-  printf("Load hits: %d\n", -1);
-  printf("Load misses: %d\n", -1);
-  printf("Store hits: %d\n", -1);
-  printf("Store misses: %d\n", -1);
-  printf("Total cycles: %d\n", -1);
+  CacheStats stats = cache.get_stats();
+
+  std::cout << "Total loads: " << stats.total_loads << std::endl;
+  std::cout << "Total stores: " << stats.total_stores << std::endl;
+  std::cout << "Load hits: " << stats.load_hits << std::endl;
+  std::cout << "Load misses: " << stats.load_misses << std::endl;
+  std::cout << "Store hits: " << stats.store_hits << std::endl;
+  std::cout << "Store misses: " << stats.store_misses << std::endl;
+  std::cout << "Total cycles: " << stats.total_cycles << std::endl;
 
   return 0;
 }
