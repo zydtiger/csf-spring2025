87fc4ea157d2a004fe21136f87be8d9b01c1e7f2
tigerding <zhiyuanding01@gmail.com>
2025-03-22 21:12:38 +0000

    only print any valid sets

--- csf_assign03/debug.cpp
+++ csf_assign03/debug.cpp
@@ -19,11 +19,20 @@ void debug_print(const Cache &c) {
   size_t tag_bits = 32 - log2(c.config.num_sets) - log2(c.config.block_size);
 
   for (int i = 0; i < c.config.num_sets; i++) {
+    bool is_set_any_valid = false;
+    for (int j = 0; j < c.config.num_blocks; j++)
+      if (c.sets[i][j].valid) {
+        is_set_any_valid = true;
+        break;
+      }
+    if (!is_set_any_valid) continue;
+
     std::cout << std::left << std::setw(5) << i;
 
     for (int j = 0; j < c.config.num_blocks; j++) {
       std::string tag_str = i_to_binarystr(c.sets[i][j].tag, tag_bits);
 
+      if (j != 0) std::cout << std::string(5, ' ');
       std::cout << " " << std::right << std::setw(40) << tag_str;
       std::cout << " " << std::right << std::setw(5) << c.sets[i][j].valid;
       std::cout << " " << std::right << std::setw(5) << c.sets[i][j].dirty;
