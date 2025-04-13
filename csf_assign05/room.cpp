6d24f24e332cb2298db20582b253daaac7215e22
tigerding <zding27@jh.edu>
2025-04-13 15:22:15 -0400

    room

--- csf_assign05/room.cpp
+++ csf_assign05/room.cpp
@@ -4,23 +4,34 @@
 #include "user.h"
 #include "room.h"
 
+#include <stdexcept>
+
 Room::Room(const std::string &room_name)
   : room_name(room_name) {
-  // TODO: initialize the mutex
+  if (pthread_mutex_init(&lock, NULL) != 0) {
+    throw std::runtime_error("Failed to initialize mutex in Room!");
+  }
 }
 
 Room::~Room() {
-  // TODO: destroy the mutex
+  pthread_mutex_destroy(&lock);
 }
 
 void Room::add_member(User *user) {
-  // TODO: add User to the room
+  Guard guard(lock);
+  members.insert(user);
 }
 
 void Room::remove_member(User *user) {
-  // TODO: remove User from the room
+  Guard guard(lock);
+  members.erase(user);
 }
 
 void Room::broadcast_message(const std::string &sender_username, const std::string &message_text) {
-  // TODO: send a message to every (receiver) User in the room
+  Guard guard(lock);
+  std::string data = room_name + ":" + sender_username + ":" + message_text;
+  Message *msg = new Message(TAG_DELIVERY, data);
+  for (User *user : members) {
+    user->mqueue.enqueue(msg);
+  }
 }
